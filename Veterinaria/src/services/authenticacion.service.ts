b48220711255981a937c9bd4a820d41b88997b98
haroldrochaucaldas <haroldrocha.tic@ucaldas.edu.co>
2022-11-05 10:26:58 -0500

    creacion de entidad credencial para el negocio

--- Veterinaria/src/services/authenticacion.service.ts
+++ Veterinaria/src/services/authenticacion.service.ts
@@ -1,10 +1,22 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
-
+import {repository} from '@loopback/repository';
+import {request} from 'http';
+import {Veterinario} from '../models';
+import {AdministradorRepository, VeterinarioRepository} from '../repositories';
+const jwt = require("jsonwebtoken")
 @injectable({scope: BindingScope.TRANSIENT})
 export class AuthenticacionService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(/* Add @inject to inject parameters */
+    @repository("AdministradorRepository")
+    public administradorRepository:AdministradorRepository,
+    @repository("VeterinarioRepository")
+    public veterinarioRepository:VeterinarioRepository
+  ) {}
 
   /*
    * Add service methods here
    */
+  IdentificarAdmintrador(usuario:string,contraseña:string){
+    
+  }
 }
